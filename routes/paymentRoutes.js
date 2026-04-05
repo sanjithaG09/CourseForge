a7abf9b14a2087951b12c42fdcef7d7046269cda
harini_git <suryaharini127@gmail.com>
2026-04-05 22:43:16 +0530

    Create paymentRoutes.js

---
+++ routes/paymentRoutes.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const router = express.Router();
+
+const auth = require("../middleware/auth");
+const {
+  createPaymentIntent,
+  handleWebhook,
+  getMyOrders
+} = require("../controllers/paymentController");
+
+router.post("/webhook", handleWebhook);
+router.post("/create", auth, createPaymentIntent);
+router.get("/my-orders", auth, getMyOrders);
+
+module.exports = router;
