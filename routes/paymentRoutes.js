8e5e3b195e92dfe0ec2aa6d3249e803713677d3b
E V Sai Chathurya <evschathurya2006@gmail.com>
2026-04-05 23:26:12 +0530

    Update paymentRoutes.js

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -8,7 +8,11 @@ const {
   getMyOrders
 } = require("../controllers/paymentController");
 
-router.post("/webhook", handleWebhook);
+router.post(
+  "/webhook",
+  express.raw({ type: "application/json" }),
+  handleWebhook
+);
 router.post("/create", auth, createPaymentIntent);
 router.get("/my-orders", auth, getMyOrders);
 
