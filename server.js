219492c39c519d7f0cadd86a48af72ae2e30a746
harini_git <suryaharini127@gmail.com>
2026-04-05 22:44:44 +0530

    Add payment routes and update server.js structure

--- server.js
+++ server.js
@@ -4,12 +4,18 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 
 const authRoutes = require("./routes/authRoutes");
-
 const courseRoutes = require("./routes/courseRoutes");
 const enrollmentRoutes = require("./routes/enrollmentRoutes");
-const searchRoutes = require("./routes/searchRoutes"); 
+const searchRoutes = require("./routes/searchRoutes");
+const paymentRoutes = require("./routes/paymentRoutes");
 
 const app = express();
+
+// Stripe webhook needs raw body
+app.use(
+  "/api/payments/webhook",
+  express.raw({ type: "application/json" })
+);
 
 // Middleware
 app.use(express.json());
@@ -17,17 +23,17 @@ app.use(cors());
 
 // Routes
 app.use("/api/auth", authRoutes);
-
 app.use("/api/courses", courseRoutes);
 app.use("/api/enrollments", enrollmentRoutes);
-app.use("/api/search", searchRoutes);  
+app.use("/api/search", searchRoutes);
+app.use("/api/payments", paymentRoutes);
 
 // DB Connection
 mongoose.connect(process.env.MONGO_URI || "mongodb://127.0.0.1:27017/courseforge")
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
 
-// Start server
+//Start Server
 app.listen(3000, () => {
   console.log("Server running on port 3000");
 });
